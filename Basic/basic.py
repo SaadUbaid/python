fae5307b2801cf20130b14e0df70dfcd2a58ec15
Saad Ubaid <saadubaid47@gmail.com>
2025-02-03 15:32:09 +0500

    Basic of python and Datatypes

---
+++ Basic/basic.py
@@ -0,0 +1,10 @@
+print('Hello World!')
+print("Hello World! this is my first program") 
+print('''Hello World!
+this is my first program''')
+
+print(2+3+4+6*100*10/100)
+
+print("i am a boy",sep='\n')
+
+print("2+3+4+6*100*10/100")
---
+++ Basic/datatype_variables.py
@@ -0,0 +1,28 @@
+# Data Types
+# Types can be use to store whcih type of data
+# int => 1-10 and so
+# fload => decinal nbrs
+# string => combunation of char
+# boolean => True and False
+# None => None
+
+#Variables
+# Variables are used to store data.
+
+a=10 # are integres
+
+b= 20.9 # Float
+
+c = "Ali" # String
+
+d = True # Boolean
+
+x= None # None type
+
+print("Value: ",a,"type is: ",type(a))
+print("Value: ",b,"type is: ",type(b))
+print("Value: ",c,"type is: ",type(c))
+print("Value: ",d,"type is: ",type(d))
+print("Value: ",x,"type is: ",type(x))
+
+
